647bb8efb47ddefd2e424d4e1402a088f9c0bb6d
THEO NAPOLY <THEO.NAPOLY@gmail.com>
2023-10-10 11:44:49 +0200

    create MyTitle.jsx - PokemonCard.jsx - modif app.jsx file to render PokemonCard Bulbasaur

---
+++ src/components/MyTitle.jsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+  return <h1>Lolipop</h1>;
+}
+
+export default MyTitle;
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,20 @@
+function PokemonCard() {
+  //state (état, données)
+
+  //comportements
+
+  //affichage (render)
+  return (
+    <>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="bulbizarre pokemon"
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </>
+  );
+}
+
+export default PokemonCard;
